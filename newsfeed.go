e3604ed276fe38338cb6b8c87b1bfc235b3c0e77
modood <mwn.china@gmail.com>
2017-12-03 12:43:33 +0800

    add include/exclude words
    wildcard pattern matching with support for '?' and '*'
    ? matches any single character
    * Matches any sequence of characters

--- newsfeed.go
+++ newsfeed.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
@@ -11,6 +12,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/modood/wpm"
 	"github.com/tidwall/gjson"
 )
 
@@ -115,30 +117,30 @@ type Owner struct {
 }
 
 // PRReviewEvent review PR
-func PRReviewEvent(item NewsFeed) {
-	user, repo := getFeedBaseInfoAndPrintAvatar(item)
+func PRReviewEvent(item NewsFeed) (string, string) {
+	user, repo, avatar := getFeedBaseInfoAndPrintAvatar(item)
 	number := item.Payload.PullRequest.Number
-	fmt.Printf("%s reviewed pull request %s on %s\n\n \a at %v", user, number, repo, item.CreatedAt)
+	return avatar, fmt.Sprintf("%s reviewed pull request %s on %s\n\n \a at %v", user, number, repo, item.CreatedAt)
 }
 
 // PREvent open PR, close PR
-func PREvent(item NewsFeed) {
-	user, repo := getFeedBaseInfoAndPrintAvatar(item)
+func PREvent(item NewsFeed) (string, string) {
+	user, repo, avatar := getFeedBaseInfoAndPrintAvatar(item)
 	state := item.Payload.PullRequest.State
 	number := item.Payload.PullRequest.Number
 	title := item.Payload.PullRequest.Title
 	body := item.Payload.PullRequest.Body
 
 	if state == "open" {
-		fmt.Printf("%s opened pull request %s on %s \n %s \n %s \a at %v\n", user, number, repo, title, body, item.CreatedAt)
+		return avatar, fmt.Sprintf("%s opened pull request %s on %s \n %s \n %s \a at %v\n", user, number, repo, title, body, item.CreatedAt)
 	} else {
-		fmt.Printf("%s closed pull request %s on %s \n %s \a at %v\n", user, number, repo, title, item.CreatedAt)
+		return avatar, fmt.Sprintf("%s closed pull request %s on %s \n %s \a at %v\n", user, number, repo, title, item.CreatedAt)
 	}
 }
 
 // comment on issue, PR
-func issueCommentEvent(item NewsFeed) {
-	user, repo := getFeedBaseInfoAndPrintAvatar(item)
+func issueCommentEvent(item NewsFeed) (string, string) {
+	user, repo, avatar := getFeedBaseInfoAndPrintAvatar(item)
 	number := item.Payload.Issue.Number
 	body := item.Payload.Comment.Body
 
@@ -149,84 +151,85 @@ func issueCommentEvent(item NewsFeed) {
 		group = "issue"
 	}
 
-	fmt.Printf("%s commented on %s %s on %s \n %s \a at %v\n", user, group, number, repo, body, item.CreatedAt)
+	return avatar, fmt.Sprintf("%s commented on %s %s on %s \n %s \a at %v\n", user, group, number, repo, body, item.CreatedAt)
 }
 
 // open issue, close issue
-func issuesEvent(item NewsFeed) {
-	user, repo := getFeedBaseInfoAndPrintAvatar(item)
+func issuesEvent(item NewsFeed) (string, string) {
+	user, repo, avatar := getFeedBaseInfoAndPrintAvatar(item)
 	state := item.Payload.Action
 	number := item.Payload.Issue.Number
 	title := item.Payload.Issue.Title
 
-	fmt.Printf("%s %s issue %s on %s \n %s \a at %v\n\n", user, state, number, repo, title, item.CreatedAt)
+	return avatar, fmt.Sprintf("%s %s issue %s on %s \n %s \a at %v\n\n", user, state, number, repo, title, item.CreatedAt)
 }
 
 // comment on a commit
-func commitCommentEvent(item NewsFeed) {
-	user, repo := getFeedBaseInfoAndPrintAvatar(item)
+func commitCommentEvent(item NewsFeed) (string, string) {
+	user, repo, avatar := getFeedBaseInfoAndPrintAvatar(item)
 	body := item.Payload.Comment.Body
-	fmt.Printf("%s commented on %s \n %s \a at %v\n", user, repo, body, item.CreatedAt)
+	return avatar, fmt.Sprintf("%s commented on %s \n %s \a at %v\n", user, repo, body, item.CreatedAt)
 }
 
 // # starred by following
-func watchEvent(item NewsFeed) {
-	user, repo := getFeedBaseInfoAndPrintAvatar(item)
-	fmt.Printf("%s starred %s \a at %v \n\n", user, repo, item.CreatedAt)
+func watchEvent(item NewsFeed) (string, string) {
+	user, repo, avatar := getFeedBaseInfoAndPrintAvatar(item)
+	return avatar, fmt.Sprintf("%s starred %s \a at %v \n\n", user, repo, item.CreatedAt)
 }
 
 // # forked by following
-func forkEvent(item NewsFeed) {
-	user, repo := getFeedBaseInfoAndPrintAvatar(item)
-	fmt.Printf("%s forked %s \a at %v\n\n", user, repo, item.CreatedAt)
+func forkEvent(item NewsFeed) (string, string) {
+	user, repo, avatar := getFeedBaseInfoAndPrintAvatar(item)
+	return avatar, fmt.Sprintf("%s forked %s \a at %v\n\n", user, repo, item.CreatedAt)
 }
 
 // # delete branch
-func deleteEvent(item NewsFeed) {
-	user, repo := getFeedBaseInfoAndPrintAvatar(item)
+func deleteEvent(item NewsFeed) (string, string) {
+	user, repo, avatar := getFeedBaseInfoAndPrintAvatar(item)
 	branch := item.Payload.Ref
-	fmt.Printf("%s deleted branch %s at \a at %v%s\n\n", user, branch, repo, item.CreatedAt)
+	return avatar, fmt.Sprintf("%s deleted branch %s at \a at %v%s\n\n", user, branch, repo, item.CreatedAt)
 }
 
 // # push commits
-func pushEvent(item NewsFeed) {
-	user, repo := getFeedBaseInfoAndPrintAvatar(item)
+func pushEvent(item NewsFeed) (string, string) {
+	user, repo, avatar := getFeedBaseInfoAndPrintAvatar(item)
 	size := item.Payload.Size
 	branch := item.Payload.Ref
-	fmt.Printf("%s pushed %d new commit(s) to %s at %s \a at %v\n\n", user, size, branch, repo, item.CreatedAt)
+	return avatar, fmt.Sprintf("%s pushed %d new commit(s) to %s at %s \a at %v\n\n", user, size, branch, repo, item.CreatedAt)
 }
 
 // # create repo, branch
-func createEvent(item NewsFeed) {
-	user, repo := getFeedBaseInfoAndPrintAvatar(item)
+func createEvent(item NewsFeed) (string, string) {
+	user, repo, avatar := getFeedBaseInfoAndPrintAvatar(item)
 	group := item.Payload.RefType
 	branch := item.Payload.Ref
 	if group == "repository" {
-		fmt.Printf("%s created %s %s \a at %v\n\n", user, group, repo, item.CreatedAt)
+		return avatar, fmt.Sprintf("%s created %s %s \a at %v\n\n", user, group, repo, item.CreatedAt)
 	} else {
-		fmt.Printf("%s created %s %s at %s \a at %v\n\n", user, group, branch, repo, item.CreatedAt)
+		return avatar, fmt.Sprintf("%s created %s %s at %s \a at %v\n\n", user, group, branch, repo, item.CreatedAt)
 	}
 }
 
 // # make public repo
-func publicEvent(item NewsFeed) {
-	user, repo := getFeedBaseInfoAndPrintAvatar(item)
-	fmt.Printf("%s made %s public \a at %v\n\n", user, repo, item.CreatedAt)
+func publicEvent(item NewsFeed) (string, string) {
+	user, repo, avatar := getFeedBaseInfoAndPrintAvatar(item)
+	return avatar, fmt.Sprintf("%s made %s public \a at %v\n\n", user, repo, item.CreatedAt)
 }
 
 // # add collab
-func memberEvent(item NewsFeed) {
+func memberEvent(item NewsFeed) (string, string) {
 	action := item.Payload.Action
 	collab := item.Payload.Member.Login
-	user, repo := getFeedBaseInfoAndPrintAvatar(item)
-	fmt.Printf("%s %s %s as a collaborator to %s \a at %v\n\n", user, action, collab, repo, item.CreatedAt)
+	user, repo, avatar := getFeedBaseInfoAndPrintAvatar(item)
+	return avatar, fmt.Sprintf("%s %s %s as a collaborator to %s \a at %v\n\n", user, action, collab, repo, item.CreatedAt)
 }
 
-// getFeedBaseInfoAndPrintAvatar 获取feed基本信息并打印用户头像
-func getFeedBaseInfoAndPrintAvatar(item NewsFeed) (string, string) {
+// getFeedBaseInfoAndPrintAvatar 获取feed基本信息
+func getFeedBaseInfoAndPrintAvatar(item NewsFeed) (string, string, string) {
 	user := item.Actor.Login
 	repo := item.Repo.Name
 	avatarURL := item.Actor.AvatarURL
+	var avatar string
 
 	if len(avatarURL) > 0 {
 		res, err := http.Get(avatarURL)
@@ -234,12 +237,12 @@ func getFeedBaseInfoAndPrintAvatar(item NewsFeed) (string, string) {
 			fmt.Printf("%s", err)
 		}
 		defer res.Body.Close()
-		display(res.Body) //
+		avatar = display(res.Body) //
 	}
-	return user, repo
+	return user, repo, avatar
 }
 
-func getReceivedEvents(user string, pageNo string) {
+func getReceivedEvents(user, pageNo, include, exclude string) {
 	url := "https://api.github.com/users/" + user + "/received_events?page=" + pageNo
 
 	startTime := time.Now()
@@ -252,62 +255,70 @@ func getReceivedEvents(user string, pageNo string) {
 
 		item := NewsFeed{}
 		json.Unmarshal([]byte(it.String()), &item)
-		if event == "PullRequestReviewCommentEvent" {
-			PRReviewEvent(item)
-		} else if event == "PullRequestEvent" {
-			PREvent(item)
-		} else if event == "IssueCommentEvent" {
-			issueCommentEvent(item)
-		} else if event == "IssuesEvent" {
-			issuesEvent(item)
-		} else if event == "CommitCommentEvent" {
-			commitCommentEvent(item)
-		} else if event == "WatchEvent" {
-			watchEvent(item)
-		} else if event == "ForkEvent" {
-			forkEvent(item)
-		} else if event == "DeleteEvent" {
-			deleteEvent(item)
-		} else if event == "PushEvent" {
-			pushEvent(item)
-		} else if event == "CreateEvent" {
-			createEvent(item)
-		} else if event == "PublicEvent" {
-			publicEvent(item)
-		} else if event == "MemberEvent" {
-			memberEvent(item)
+		var content, avatar string
+
+		switch event {
+		case "PullRequestReviewCommentEvent":
+			avatar, content = PRReviewEvent(item)
+		case "PullRequestEvent":
+			avatar, content = PREvent(item)
+		case "IssueCommentEvent":
+			avatar, content = issueCommentEvent(item)
+		case "IssuesEvent":
+			avatar, content = issuesEvent(item)
+		case "CommitCommentEvent":
+			avatar, content = commitCommentEvent(item)
+		case "WatchEvent":
+			avatar, content = watchEvent(item)
+		case "ForkEvent":
+			avatar, content = forkEvent(item)
+		case "DeleteEvent":
+			avatar, content = deleteEvent(item)
+		case "PushEvent":
+			avatar, content = pushEvent(item)
+		case "CreateEvent":
+			avatar, content = createEvent(item)
+		case "PublicEvent":
+			avatar, content = publicEvent(item)
+		case "MemberEvent":
+			avatar, content = memberEvent(item)
+		default: // do nothing
 		}
+
+		output(avatar, content, include, exclude)
 	}
 }
 
 // ReceivedEvents get received events
-func ReceivedEvents(user string, maxPage int) {
+func ReceivedEvents(user string, maxPage int, include, exclude string) {
 	for page := 1; page <= maxPage; page++ {
-		getReceivedEvents(user, fmt.Sprintf("%d", page))
+		getReceivedEvents(user, fmt.Sprintf("%d", page), include, exclude)
 	}
 }
 
 // display 控制台打印图片
-func display(r io.Reader) {
+func display(r io.Reader) string {
 	data, err := ioutil.ReadAll(r)
 	if err != nil {
 	}
 
 	width, height := widthAndHeight()
 
-	fmt.Print("\033]1337;")
-	fmt.Printf("File=inline=1")
-	if width != "" || height != "" {
-		if width != "" {
-			fmt.Printf(";width=%s", width)
-		}
-		if height != "" {
-			fmt.Printf(";height=%s", height)
-		}
+	var buf bytes.Buffer
+	buf.WriteString("\033]1337;File=inline=1")
+
+	if width != "" {
+		buf.WriteString(";width=")
+		buf.WriteString(width)
 	}
-	fmt.Print(":")
-	fmt.Printf("%s", base64.StdEncoding.EncodeToString(data))
-	fmt.Print("\a")
+	if height != "" {
+		buf.WriteString(";height=")
+		buf.WriteString(height)
+	}
+	buf.WriteString(":")
+	buf.WriteString(base64.StdEncoding.EncodeToString(data))
+	buf.WriteString("\a")
+	return buf.String()
 }
 
 func widthAndHeight() (w, h string) {
@@ -325,4 +336,12 @@ func widthAndHeight() (w, h string) {
 		}
 	}
 	return
+}
+
+func output(avatar, content, include, exclude string) {
+	if (len(exclude) > 0 && wpm.WildcardPatternMatch(content, "*"+exclude+"*")) ||
+		(len(include) > 0 && !wpm.WildcardPatternMatch(content, "*"+include+"*")) {
+		return
+	}
+	fmt.Print(avatar, content)
 }
